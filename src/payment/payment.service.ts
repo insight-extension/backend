274c25cc677eb8f43210dbfb4573271b94d4a94a
Artiom Sopin <artiomsopin@proton.me>
2024-12-20 00:50:25 +0200

    feat: add basic payment per time logic

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,26 +1,80 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import * as anchor from '@coral-xyz/anchor';
-import { Program } from '@coral-xyz/anchor';
-import { DepositProgram } from './interfaces/deposit_program';
-import { clusterApiUrl, Connection } from '@solana/web3.js';
-import { IDL } from '@coral-xyz/anchor/dist/cjs/native/system';
+import {
+  AnchorProvider,
+  Program,
+  setProvider,
+  Wallet,
+} from '@coral-xyz/anchor';
+import type { DepositProgram } from './interfaces/deposit_program';
+import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
 import 'dotenv/config';
+import * as idl from './interfaces/deposit_program.json';
+import { TOKEN_PROGRAM_ID } from '@coral-xyz/anchor/dist/cjs/utils/token';
 
 @Injectable()
 export class PaymentService implements OnModuleInit {
-  private readonly provider;
-  private readonly connection;
-  private readonly program;
-  private readonly PROGRAM_ID = process.env.PROGRAM_ID;
+  private readonly acnhorProvider: AnchorProvider;
+  private readonly connection: Connection;
+  private readonly program: Program<DepositProgram>;
+  private readonly walletForProvider: Wallet;
+  private readonly master: Keypair;
+  private readonly tokenProgram = TOKEN_PROGRAM_ID;
+  private readonly tokenUSDC = new PublicKey(
+    '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU',
+  );
+  // TODO: Replace with actual user keypair
+  private readonly userKeypair: Keypair = new Keypair();
 
   constructor() {
     // Setup config
-    this.provider = anchor.AnchorProvider.env();
-    anchor.setProvider(this.provider);
+    this.master = Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse(process.env.MASTER_KEY ?? '')),
+    );
     this.connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-    // this.program = new Program<DepositProgram>(IDL, PROGRAM_ID, {
-    //   connection: this.connection,
-    // });
+    this.walletForProvider = new Wallet(this.userKeypair);
+    this.acnhorProvider = new AnchorProvider(
+      this.connection,
+      this.walletForProvider,
+      AnchorProvider.defaultOptions(),
+    );
+    setProvider(this.acnhorProvider);
+    this.program = new Program(idl as DepositProgram, this.acnhorProvider);
+  }
+
+  private async depositToTimedVault() {
+    try {
+      const transaction = await this.program.methods
+        .depositToTimedVault(new anchor.BN(1_000_000))
+        .accounts({
+          user: this.userKeypair.publicKey,
+          token: this.tokenUSDC,
+          tokenProgram: this.tokenProgram,
+        })
+        .signers([this.userKeypair])
+        .rpc();
+      console.log(transaction);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
+  private async payPerTime() {
+    try {
+      const transaction = await this.program.methods
+        // TODO: Implement the function of converting to the correct format
+        .payPerTime(new anchor.BN(1_000_000))
+        .accounts({
+          user: this.userKeypair.publicKey,
+          token: this.tokenUSDC,
+          tokenProgram: this.tokenProgram,
+        })
+        .signers([this.master])
+        .rpc();
+      console.log(transaction);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   onModuleInit() {
